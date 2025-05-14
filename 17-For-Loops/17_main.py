78ce325a59a370d1b378de938e1a77a83bce39ef
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-05-14 14:54:21 +0530

    Update 17_main.py

--- 17-For-Loops/17_main.py
+++ 17-For-Loops/17_main.py
@@ -1,20 +1,20 @@
-# name = 'Abhishek'
-# for i in name:
-#   print(i)
-#   if(i =="b"):
-#     print("This is something special!")
+name = 'Abhishek'
+for i in name:
+  print(i)
+  if(i =="b"):
+    print("This is something special!")
     
-# colors = ["Red", "Green", "Blue", "Yellow"]
-# for color in colors:
-#   print(color)
-#   for i in color:
-#     print(i)
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
 
-# for k in range(5):
-#   print(k + 1)
+for k in range(5):
+  print(k + 1)
   
-# for k in range(1, 20001):
-#   print(k)
+for k in range(1, 20001):
+  print(k)
 
   
 for k in range(1, 12, 3):
