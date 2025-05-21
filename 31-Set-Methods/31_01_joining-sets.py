d980bcab288069ac6946ab223d08e4b19116115d
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-05-21 17:11:55 +0530

    commit

---
+++ 31-Set-Methods/31_01_joining-sets.py
@@ -0,0 +1,42 @@
+A={1,2,3,4}
+B={3,4,7,6}
+
+#1
+# C=A.union(B)
+# print(C)
+# {1, 2, 3, 4, 6, 7}
+
+#2
+# A.update(B)
+# print(A)
+# {1, 2, 3, 4, 6, 7}
+
+#3
+# C=A.intersection(B)
+# print(C)
+# {3, 4}
+
+#4
+# A.intersection_update(B)
+# print(A)
+# {3, 4}
+
+#5
+# C= A.symmetric_difference(B)
+# print(C)
+# {1, 2, 6, 7}
+
+#6
+# A.symmetric_difference_update(B)
+# print(A)
+# {1, 2, 6, 7}
+
+#7
+# C=A.difference(B)
+# print(C)
+# {1, 2}
+
+#8
+# A.difference_update(B)
+# print(A)
+# {1, 2}
