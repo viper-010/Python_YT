804151333758c3bd43ae4ba89f7c4eb37a0936ba
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-06-25 16:59:09 +0530

    Update 06-Variables-&-Data-Types.py

--- 06-Variables-and-Data-Types/06-Variables-&-Data-Types.py
+++ 06-Variables-and-Data-Types/06-Variables-&-Data-Types.py
@@ -9,6 +9,7 @@ print(a + a1)
 print("The type of a is ", type(a))
 print("The type of b is ", type(b))
 print("The type of c is ", type(c))
+print("The type of d is ", type(d))
 
 
 list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
