3fa044071b049b4fad85180e46bc456e3b2bdbb4
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-07-04 17:23:10 +0530

    Update 15_Ex2_solution.py

--- 15-Exercise-2-Good-Morning/15_Ex2_solution.py
+++ 15-Exercise-2-Good-Morning/15_Ex2_solution.py
@@ -8,7 +8,7 @@ if(hour>=0 and hour<12):
   print("Good Morning Sir!")
 elif(hour>=12 and hour<17):
   print("Good Afternoon Sir!")
-elif(hour>=17 and hour<0):
+elif(hour>=17 and hour,<=23):
   print("Good Night Sir!")
 
 
