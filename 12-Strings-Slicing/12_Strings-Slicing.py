b2051496dbd74aa191b52fe236344bf0b18ed3aa
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-07-17 16:55:58 +0530

    Update 12_Strings-Slicing.py

--- 12-Strings-Slicing/12_Strings-Slicing.py
+++ 12-Strings-Slicing/12_Strings-Slicing.py
@@ -9,6 +9,7 @@ print(fruit[:5]) # if not assumed, assune it as [0:5]
 print(fruit[0:-3]) # taken as from [0:2]
 print(fruit[:len(fruit)-3]) # taken as from [0:2]
 print(fruit[-1:len(fruit) - 3]) # taken as from [4,2]
+#Rule of Slicing: Python tries to go from the start index up to (but not including) the end index by stepping forward one character at a time,but not backwards.
 print(fruit[-3:-1]) # takes as [2:4]
 print(fruit[-4:-2]) # taken as [1:3]
 
