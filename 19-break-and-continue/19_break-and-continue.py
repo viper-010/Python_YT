fffe723aaebaaec26c6ff86babcf0a4826d7db9e
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-07-22 14:25:27 +0530

    Update 19_break-and-continue.py

--- 19-break-and-continue/19_break-and-continue.py
+++ 19-break-and-continue/19_break-and-continue.py
@@ -1,9 +1,11 @@
+## for continue
 # for i in range(12):
 #   if(i == 10):
 #     print("Skip the iteration")
 #     continue
 #   print("5 X", i, "=", 5 * i)
-  
+
+## for break
 i = 0
 while True:
   print(i)
