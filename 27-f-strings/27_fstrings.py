e15bf2cac08cab1fd990a09eadb96b511c3f715e
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-08-01 12:46:32 +0530

    Update 27_fstrings.py

--- 27-f-strings/27_fstrings.py
+++ 27-f-strings/27_fstrings.py
@@ -3,10 +3,18 @@ country = "India"
 name = "Harry"
 
 print(letter.format(country, name))
+# index                0   ,  1
+
 print(f"Hey my name is {name} and I am from {country}")
+
+# If you want the flower brackets
 print(f"We use f-strings like this: Hey my name is {{name}} and I am from {{country}}")
+
 price = 49.09999
 txt = f"For only {price:.2f} dollars!"
 print(txt)
-# print(txt.format())
+# doesn't do anything since the txt string has no placeholders
+print(txt.format())
+
 print(type(f"{2 * 30}"))
+print(f"{2*30}")
