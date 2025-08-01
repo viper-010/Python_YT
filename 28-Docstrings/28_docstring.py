fb6bd0f47ebd90667572140cb8ed25f692429eaa
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-08-01 15:22:54 +0530

    Update 28_docstring.py

--- 28-Docstrings/28_docstring.py
+++ 28-Docstrings/28_docstring.py
@@ -1,5 +1,11 @@
+'''This is a module for demonstrating docstrings.'''
+
 def square(n):
-  '''Takes in a number n, returns the square of n'''
-  print(n**2)
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+
 square(5)
 print(square.__doc__)
+
+#A module docstring must be a string literal placed as the very first statement in the file, before any imports or code.
+print(__doc__)
