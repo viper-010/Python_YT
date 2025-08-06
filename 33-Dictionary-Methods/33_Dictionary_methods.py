20446840d466ea91b11b6d51a52927aa5e9f80ee
Rohan T A <rohanta.1si21ec077@gmail.com>
2025-08-06 10:12:49 +0530

    Update 33_Dictionary_methods.py

--- 33-Dictionary-Methods/33_Dictionary_methods.py
+++ 33-Dictionary-Methods/33_Dictionary_methods.py
@@ -1,9 +1,13 @@
-ep1 = {122: 45, 123: 89, 567: 69, 670: 69}
+ep1 = {122: 45, 123: 89, 567: 69, 670: 69, 566: 85}
 ep2 = {222: 67, 566: 90}
 
-# ep1.update(ep2)
-# ep1.clear()
-# ep1.pop(122)
-ep1.popitem()
-del ep1[122]
+ep1.update(ep2)
 print(ep1) 
+# ep1.clear()
+# print(ep1) 
+# ep1.pop(122)
+# print(ep1) 
+# ep1.popitem()
+# print(ep1) 
+# del ep1[122]
+# print(ep1) 
